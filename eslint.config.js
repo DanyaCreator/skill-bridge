27b8752261cc78e5f949f2114a9a6fa860e00fb2
mhlw <mihajlow.dima258@gmail.com>
2025-10-09 01:09:42 +0300

    chore: setup ESLint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
 import prettier from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -16,15 +18,59 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      perfectionist.configs['recommended-natural'],
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: { prettier },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      prettier,
+    },
     rules: {
-      ...eslintConfigPrettier.rules,
-      'prettier/prettier': 'error',
+      'perfectionist/sort-objects': 'off',
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', next: 'return', prev: '*' },
+        { blankLine: 'always', next: '*', prev: ['const', 'let', 'var'] },
+        {
+          blankLine: 'any',
+          next: ['const', 'let', 'var'],
+          prev: ['const', 'let', 'var'],
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000', '^react', '^@?\\w'],
+
+            [
+              '^@/app$',
+              '^@/features(/.*)?$',
+              '^@/entities(/.*)?$',
+              '^@/shared(/.*)?$',
+            ],
+
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            ['\\.css$'],
+          ],
+        },
+      ],
+
+      'simple-import-sort/exports': 'error',
     },
   },
 ]);
